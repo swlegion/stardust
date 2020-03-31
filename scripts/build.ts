03c7c51f9d5d3fc26d0ac369588026618b9a7f2c
Matan Lurey <matan@lurey.org>
2020-03-31 15:16:24 -0700

    Augment build script to support global XML and UI includes.

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,6 +4,7 @@ import chalk from 'chalk';
 import fs from 'fs-extra';
 import path from 'path';
 import { exit } from 'shelljs';
+import { loadAndRewriteJson } from '../lib/tts_json_helper';
 
 function onCleanup(): void {
   exit(0);
@@ -14,10 +15,13 @@ process.on('SIGINT', onCleanup);
 console.log(chalk.magentaBright('\nStardust Development Tool\n'));
 console.log('Starting up build process...');
 
-console.log('Copying mod...');
+console.log('Copying and re-writing mod...');
 const modInSourceTree = path.join('src', 'mod', 'Stardust.json');
 const modInOutputDir = path.join('.build', 'stardust', 'Stardust.json');
-fs.copyFileSync(modInSourceTree, modInOutputDir);
+
+const modJson = fs.readFileSync(modInSourceTree, { encoding: 'UTF-8' });
+const outputJson = loadAndRewriteJson(modJson);
+fs.writeFileSync(modInOutputDir, outputJson);
 
 console.log(chalk.magentaBright('Done!\n'));
 
