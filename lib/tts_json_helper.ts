273c8720c1924da13483e6ca82b6f905a9e66b8e
Matan Lurey <matan@lurey.org>
2020-04-01 20:10:04 -0700

    Remove unused code.

--- lib/tts_json_helper.ts
+++ lib/tts_json_helper.ts
@@ -2,39 +2,11 @@ import fs from 'fs-extra';
 import glob from 'glob';
 import path from 'path';
 import { DataBlob, Unit } from './types/schema';
-import { TTSBase, TTSMod } from './types/tts_json';
 
 const rewriteBasePath = `file://${path.posix.resolve('.')}/`.replace(
   /\\/g,
   '/',
 );
-
-function rewriteInclude(include: string): string {
-  const lines = include.split('\n');
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    if (line.indexOf('#include ') === 0) {
-      const include = line.substring('#include '.length);
-      let relative = path.join('src', include);
-      if (path.extname(relative) === '') {
-        relative = `${relative}.ttslua`;
-      }
-      const loaded = fs.readFileSync(relative, { encoding: 'UTF-8' });
-      lines[i] = rewriteInclude(loaded);
-    } else if (line.indexOf('~#/') !== -1) {
-      lines[i] = line.replace('~#/', rewriteBasePath);
-    }
-  }
-  return lines.join('\n');
-}
-
-function rewriteIncludes(object: TTSBase): void {
-  const lua = object.LuaScript;
-  object.LuaScript = rewriteInclude(lua);
-  const xml = object.XmlUI;
-  object.XmlUI = rewriteInclude(xml);
-}
-
 /**
  * Loads and concatenates all of the JSON data in `data/` into a single blob.
  */
@@ -60,18 +32,4 @@ export function concatAndMergeData(
     blob.units.push(rewrittenUnit);
   }
   return JSON.stringify(blob, null, options.format ? '  ' : undefined);
-}
-
-/**
- * Loads the provided string as a mod, and returns it re-written as needed.
- *
- * @param json Full JSON string of the mod file.
- */
-export function loadAndRewriteJson(
-  json: string,
-  options: { format: boolean } = { format: true },
-): string {
-  const mod = JSON.parse(json) as TTSMod;
-  rewriteIncludes(mod);
-  return JSON.stringify(mod, null, options.format ? '  ' : undefined);
 }
