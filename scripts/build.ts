7dc66ea2ac4ce0779d4a5ed63f107a39c5cdef75
Matan Lurey <matan@lurey.org>
2020-04-05 12:49:50 -0700

    Deploy data workflow.

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,7 +3,7 @@
 import chalk from 'chalk';
 import fs from 'fs-extra';
 import path from 'path';
-import { exit } from 'shelljs';
+import shell, { exit } from 'shelljs';
 import { ModRepoMapper } from '../lib/mod/mapper';
 import { createDevLink, findSavesDir } from '../lib/mod/tools';
 import { concatAndMergeData } from '../lib/tts_json_helper';
@@ -24,7 +24,7 @@ createDevLink(savesDir);
 const modInSourceTree = path.join('src');
 console.log('Copying and re-building mod from', modInSourceTree);
 
-const modInOutputDir = path.join('.build', 'stardust', 'Stardust.json');
+const modInOutputDir = path.join('.build', 'mod', 'stardust.json');
 const mapper = new ModRepoMapper();
 const modMetaTree = mapper.readMapSync(modInSourceTree, 'global');
 
@@ -37,6 +37,13 @@ const mergedJson = concatAndMergeData();
 const jsonInOutputDir = path.join('.build', 'data.json');
 fs.writeFileSync(jsonInOutputDir, mergedJson);
 
-console.log(chalk.magentaBright('Done!\n'));
+console.log('Preparing HTML...');
+const inputHtml = fs.readFileSync('index.html', { encoding: 'UTF-8' });
+const gitCommit = shell.exec('git rev-parse --short HEAD', { silent: true });
+const replaceHtml = inputHtml.replace(/\{\%COMMIT\%\}/g, gitCommit);
+const htmlInOutputDir = path.join('.build', 'index.html');
+fs.writeFileSync(htmlInOutputDir, replaceHtml);
+
+console.log(chalk.magentaBright('\nDone!\n'));
 
 onCleanup();
