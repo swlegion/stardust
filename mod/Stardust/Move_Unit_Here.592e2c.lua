47783867dcd51b035e57a725eb8fc2b1fed97c1f
Matan Lurey <matan@lurey.org>
2020-05-14 18:13:26 -0700

    Tweak again.

--- mod/Stardust/Move_Unit_Here.592e2c.lua
+++ mod/Stardust/Move_Unit_Here.592e2c.lua
@@ -1,17 +1,22 @@
 --- A model that projects a ghost miniature to move another miniature to.
 --
+-- Projects UI that, when interacted with, moves the provided model (by GUID).
+--
 -- Expects a GUID to be provided via the field 'setupUnitProxy`:
 --
--- @usage
---   token.setTable('setupUnitProxy', 'abc123')
---
--- Projects UI that, when interacted with, moves the provided model (by GUID).
+-- @see Move_Unit_Here:setupUnitProxy
 --
 -- @module Move_Unit_Here
 
 _PERSIST = {
   MOVE_GUID = nil,
 }
+
+--- A GUID that is expected to be setup in order to display relevant UI.
+--
+-- @usage
+--   token.setTable('setupUnitProxy', 'abc123')
+setUpUnitProxy = nil
 
 function onLoad(state)
   if state != '' then
