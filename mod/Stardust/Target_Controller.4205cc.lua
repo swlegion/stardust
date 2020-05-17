b956906397982bc5d0b9e1800796db24fa3e7392
Matan Lurey <matan@lurey.org>
2020-05-16 21:02:35 -0700

    Add threat-mode range finder.

--- mod/Stardust/Target_Controller.4205cc.lua
+++ mod/Stardust/Target_Controller.4205cc.lua
@@ -13,6 +13,29 @@ _RANGE_FINDERS = {
   Small = 'https://assets.swlegion.dev/tools/ranges/small.unity3d',
 }
 
+--- Colors used in the range finder for a given range.
+--
+-- @local
+_RANGE_FINDER_COLORS = {
+  -- 1
+  '#FF0000',
+
+  -- 2
+  '#0000FF',
+
+  -- 3
+  '#FF6D00',
+
+  -- 4
+  '#00FF00',
+
+  -- 5
+  '#FFFF00',
+
+  -- 6+
+  '#808080',
+}
+
 function onLoad()
   drawTemporaryUI()
 end
@@ -92,6 +115,166 @@ function spawnRangeFinder(args)
   object.interactable = false
   object.setScale({0, 0, 0})
   return object
+end
+
+--- Clears floating numbers from all miniatures on the table.
+function clearFloatingNumbers()
+  local table = getObjectFromGUID(
+    Global.getTable('GUIDS').objects.Table
+  )
+  local minis = table.call('getAllTargets')
+  for _, mini in ipairs(minis) do
+    mini.call('hideFloatingNumber')
+  end
+end
+
+--- Returns all miniatures owned by the opposing team.
+--
+-- @param args A table with the field `team` making the request.
+--
+-- @return List of miniatures.
+--
+-- @usage
+-- local allEnemies = target.call('findAllEnemyMinis', {
+--   team: 'Blue',
+-- })
+-- for _, mini in allEnemies do
+--   -- Do something.
+-- end
+function findAllEnemyMinis(args)
+  local results = {}
+  local team = args.team
+  local area = getObjectFromGUID(
+    Global.getTable('GUIDS').objects.Table
+  )
+  local minis = area.call('getAllTargets')
+  for _, mini in ipairs(minis) do
+    local owner = mini.call('getOwner')
+    if owner != team then
+      table.insert(results, mini)
+    end
+  end
+  return results
+end
+
+--- Returns all miniatures owned by the opposing team, grouped into units.
+--
+-- @param args A table with the field `team` making the request.
+--
+-- @return List of list of miniatures.
+--
+-- @usage
+-- local allEnemies = target.call('findAllEnemyMinis', {
+--   team: 'Blue',
+-- })
+-- for _, unitGroup in allEnemies do
+--   local leader = unitGroup[1]
+--   -- Other units are not the leader.
+-- end
+function groupAllEnemyMinis(args)
+  local groups = {}
+  local team = args.team
+  local area = getObjectFromGUID(
+    Global.getTable('GUIDS').objects.Table
+  )
+  local minis = area.call('getAllTargets')
+  -- TODO: Optimize this.
+  for _, mini in ipairs(minis) do
+    local owner = mini.call('getOwner')
+    if owner != team and not mini.getVar('IS_UNIT_LEADER') then
+      local leader = mini.call('getUnitLeader')
+      if groups[leader] then
+        table.insert(groups[leader], mini)
+      else
+        groups[leader] = {leader, mini}
+      end
+    end
+  end
+  local results = {}
+  for _, group in ipairs(groups) do
+    table.insert(results, group)
+  end
+  return results
+end
+
+--- Returns the range, in game terms (e.g. 2D horizontal only), in inches.
+--
+-- @param args A table with the field `from` and `to`, each with `position`, `rotation`, `bounds`.
+--
+-- @return Distance, in inches.
+--
+-- @usage
+-- local distance = target.call('computeDistance', {
+--   from = {
+--     position = from.getPosition(),
+--     rotation = from.getRotation(),
+--     bounds   = from.getBounds().size,
+--   },
+--   to = {
+--     position = to.getPosition(),
+--     rotation = to.getRotation(),
+--     bounds   = to.getBounds().size,
+--   }
+-- })
+function computeDistance(args)
+  -- TODO: Implement correctly as a purely horizontal range-check, and make
+  -- sure to use the tip of the base for calculations - this is a basically
+  -- an estimate.
+  return _distance2D(
+    args.from.position.x,
+    args.from.position.z,
+    args.to.position.x,
+    args.to.position.z
+  )
+end
+
+function _distance2D(x1, y1, x2, y2)
+  local dx = x1 - x2
+  local dy = y1 - y2
+  return math.sqrt(dx * dx + dy * dy)
+end
+
+--- Shows all ranges of enemy minis of the provided mini.
+--
+-- @param args A table with the field `origin`.
+--
+-- @usage
+-- target.call('showRangeOfEnemyMinis', {
+--   origin = myMini,
+-- })
+function showRangeOfEnemyMinis(args)
+  local origin = args.origin
+  local target = getObjectFromGUID(
+    Global.getTable('GUIDS').controllers.Target
+  )
+  local allEnemyMinis = target.call('findAllEnemyMinis', {
+    team = origin.call('getOwner')
+  })
+  local position = origin.getPosition()
+  local rotation = origin.getRotation()
+  local bounds = origin.getBounds().size
+  for _, enemy in ipairs(allEnemyMinis) do
+    local distance = target.call('computeDistance', {
+      from = {
+        position = position,
+        rotation = rotation,
+        bounds = bounds,
+      },
+
+      to = {
+        position = enemy.getPosition(),
+        rotation = enemy.getRotation(),
+        bounds = enemy.getBounds().size,
+      }
+    })
+
+    -- Translate to in-game ranges.
+    distance = math.min(6, math.ceil(distance / 6))
+    enemy.call('showFloatingNumber', {
+      number = distance,
+      color = _RANGE_FINDER_COLORS[distance],
+    })
+  end
 end
 
 --- Does a LOS check for the selected unit leaders.
